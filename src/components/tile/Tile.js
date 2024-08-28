96b47a984bc394b92c3d0b1685a7d708effb10c8
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-08-28 10:36:07 +0200

    complete Tile

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ name, description }) => {
+  const array = Object.values(description)
   return (
     <div className="tile-container">
-      
+      <p>{name}</p>
+      {array.map((desc, index) => {
+        return <p key={index}>{desc}</p>
+      })}
     </div>
   );
 };
