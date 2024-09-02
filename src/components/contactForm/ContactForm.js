a51627000b56828b0b6143fc0cfdd52f16d3f319
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-09-02 17:16:23 +0200

    fix the last submit bug and added placeholders for clarity

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -13,12 +13,15 @@ export const ContactForm = ({
     <>
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name</label>
-        <input id="name" type="text" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+        <input id="name" type="text" name="name" 
+        placeholder="Insert Name" value={name} onChange={(e) => setName(e.target.value)}/>
         <label htmlFor="phone">Phone</label>
-        <input id="phone" type="tel" name="phone" value={phone} onChange={(e) => setPhone(e.target.value)}
+        <input id="phone" type="tel" name="phone" 
+        placeholder="Insert Phone ###-###-####" value={phone} onChange={(e) => setPhone(e.target.value)}
         pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"/>
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <input id="email" type="email" name="email" 
+        placeholder="Insert a valid Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
         <input type="submit" value="Submit" />
       </form>
     </>
